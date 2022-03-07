c0d6d277ac63350eff9612fcd13dea7618439bae
Rodrigo Bervig <rodrigo_bervig@hotmail.com>
2022-03-07 15:29:52 -0300

    Add simple corner evaluation function and format file

--- Project 2 Alpha Beta Pruning in Reversi/kit_othello/advsearch/r2d2/agent.py
+++ Project 2 Alpha Beta Pruning in Reversi/kit_othello/advsearch/r2d2/agent.py
@@ -17,29 +17,62 @@ def make_move(board: board.Board, agent_color: str) -> tuple[int, int]:
     """
     global INITIAL_TIME
     INITIAL_TIME = time()
-    
+
     # print(board.legal_moves(color))
-    possible_moves: list[tuple[int,int]] = get_ordered_possible_moves(board, agent_color)
+    possible_moves: list[tuple[int, int]] = get_ordered_possible_moves(
+        board, agent_color)
     best_move = get_best_move(board.__str__(), possible_moves, agent_color)
     print(time() - INITIAL_TIME)
     return best_move
 
-def get_ordered_possible_moves(board: board.Board, color: str) -> list[tuple[int,int]]:
+
+def get_ordered_possible_moves(board: board.Board,
+                               color: str) -> list[tuple[int, int]]:
 
     ordered_moves = board.legal_moves(color)
 
     return ordered_moves
+
+
+def corner(board: board.Board, max_agent_color: str) -> int:
+    oponent_color = board.opponent(max_agent_color)
+    agent_corners = 0
+    oponent_corners = 0
+
+    if board[0][0] == max_agent_color:
+        agent_corners += 1
+    if board[7][0] == max_agent_color:
+        agent_corners += 1
+    if board[0][7] == max_agent_color:
+        agent_corners += 1
+    if board[7][7] == max_agent_color:
+        agent_corners += 1
+
+    if board[0][0] == oponent_color:
+        oponent_corners += 1
+    if board[7][0] == oponent_color:
+        oponent_corners += 1
+    if board[0][7] == oponent_color:
+        oponent_corners += 1
+    if board[7][7] == oponent_color:
+        oponent_corners += 1
+
+    return (0 if oponent_corners + agent_corners == 0 else
+            (agent_corners - oponent_corners) /
+            (oponent_corners + agent_corners))
+
 
 def heuristic(board: board.Board, agent_color: str) -> int:
     points = get_points(board, agent_color)
 
     if board.is_terminal_state():
         if points[0] > points[1]:
-            return inf # win
+            return inf  # win
         elif points[0] < points[1]:
-            return -inf # loss
-    
+            return -inf  # loss
+
     return points[0] - points[1]
+
 
 def get_points(board: board.Board, agent_color: str) -> tuple[int, int]:
     """
@@ -52,8 +85,9 @@ def get_points(board: board.Board, agent_color: str) -> tuple[int, int]:
     return (p1_score, p2_score)
 
 
-def get_best_move(cur_state: str, possible_moves: list[tuple[int,int]], agent_color: str) -> tuple[int,int]:
-    best_move = (-1,-1)
+def get_best_move(cur_state: str, possible_moves: list[tuple[int, int]],
+                  agent_color: str) -> tuple[int, int]:
+    best_move = (-1, -1)
     best_value = -inf
 
     alpha = -inf
@@ -61,7 +95,8 @@ def get_best_move(cur_state: str, possible_moves: list[tuple[int,int]], agent_co
     for move in possible_moves:
         move_board = board.from_string(cur_state)
         move_board.process_move(move, agent_color)
-        move_value = get_min_value(move_board.__str__(), alpha, beta, move_board.opponent(agent_color), 1)
+        move_value = get_min_value(move_board.__str__(), alpha, beta,
+                                   move_board.opponent(agent_color), 1)
         if move_value > best_value:
             best_value = move_value
             best_move = move
@@ -69,14 +104,16 @@ def get_best_move(cur_state: str, possible_moves: list[tuple[int,int]], agent_co
         alpha = max(alpha, best_value)
         if alpha >= beta:
             break
-    
+
     if best_value == -inf:
         # if all moves are losing moves (it's impossible to win), choose a random move
         best_move = random.choice(possible_moves)
 
     return best_move
 
-def get_max_value(cur_state: str, alpha: float, beta: float, agent_color: str, cur_depth: int) -> int:
+
+def get_max_value(cur_state: str, alpha: float, beta: float, agent_color: str,
+                  cur_depth: int) -> int:
     cur_board = board.from_string(cur_state)
 
     if cur_depth >= MAX_DEPTH or time() - INITIAL_TIME > MAX_TIME_IN_SECONDS:
@@ -97,7 +134,10 @@ def get_max_value(cur_state: str, alpha: float, beta: float, agent_color: str, c
     for move in legal_moves:
         move_board = board.from_string(cur_state)
         move_board.process_move(move, agent_color)
-        v = max(v, get_min_value(move_board.__str__(), alpha, beta, opponent, cur_depth + 1))
+        v = max(
+            v,
+            get_min_value(move_board.__str__(), alpha, beta, opponent,
+                          cur_depth + 1))
         alpha = max(alpha, v)
         if alpha >= beta:
             break
@@ -105,7 +145,8 @@ def get_max_value(cur_state: str, alpha: float, beta: float, agent_color: str, c
     return v
 
 
-def get_min_value(cur_state: str, alpha: float, beta: float, opponent_color: str, cur_depth: int) -> int:
+def get_min_value(cur_state: str, alpha: float, beta: float,
+                  opponent_color: str, cur_depth: int) -> int:
     cur_board = board.from_string(cur_state)
 
     agent_color = cur_board.opponent(opponent_color)
@@ -120,16 +161,20 @@ def get_min_value(cur_state: str, alpha: float, beta: float, opponent_color: str
             # print('estado terminal min', heuristic(cur_board, agent_color))
             return heuristic(cur_board, agent_color)
         else:
-            return get_max_value(cur_state, alpha, beta, agent_color, cur_depth)
+            return get_max_value(cur_state, alpha, beta, agent_color,
+                                 cur_depth)
 
     v = inf
     for move in legal_moves:
         move_board = board.from_string(cur_state)
         move_board.process_move(move, opponent_color)
-        v = min(v, get_max_value(move_board.__str__(), alpha, beta, agent_color, cur_depth + 1))
+        v = min(
+            v,
+            get_max_value(move_board.__str__(), alpha, beta, agent_color,
+                          cur_depth + 1))
 
         beta = min(beta, v)
-        if beta <= alpha: 
+        if beta <= alpha:
             break
 
     return v
